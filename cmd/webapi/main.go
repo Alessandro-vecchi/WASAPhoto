f425cc6ca2ae5537b1ccd61ee4d76610321b5bc2
Alessandro Vecchi <vecchi.1938069@studenti.uniroma1.it>
2022-12-03 16:39:11 +0100

    initialized go server

--- cmd/webapi/main.go
+++ cmd/webapi/main.go
@@ -28,18 +28,19 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
-	"git.sapienzaapps.it/gamificationlab/wasa-homework-enroll/service/api"
-	"git.sapienzaapps.it/gamificationlab/wasa-homework-enroll/service/database"
-	"github.com/ardanlabs/conf"
-	"github.com/gorilla/handlers"
-	_ "github.com/mattn/go-sqlite3"
-	"github.com/sirupsen/logrus"
 	"math/rand"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/Alessandro-vecchi/WASAPhoto/service/api"
+	"github.com/Alessandro-vecchi/WASAPhoto/service/database"
+	"github.com/ardanlabs/conf"
+	"github.com/gorilla/handlers"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/sirupsen/logrus"
 )
 
 // main is the program entry point. The only purpose of this function is to call run() and set the exit code if there is
