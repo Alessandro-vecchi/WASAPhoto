83decbde0e54b0847d26a462476e932c8d27f8fb
Alessandro Vecchi <vecchi.1938069@studenti.uniroma1.it>
2022-12-12 19:43:20 +0100

    ordered photos reversed

--- service/database/get-list-photos.go
+++ service/database/get-list-photos.go
@@ -3,11 +3,12 @@ package database
 func (db *appdbimpl) GetListUserPhotos(user_id string) ([]Photo_db, error) {
 
 	const query = `
-SELECT *
-FROM photos
-WHERE user_id =?`
+		SELECT *
+		FROM photos
+		WHERE user_id = ?
+		ORDER BY timestamp DESC;`
 
-	var ret []Photo_db
+	var photos []Photo_db
 
 	// Issue the query, using the user_id as filter
 	rows, err := db.c.Query(query,
@@ -17,19 +18,19 @@ WHERE user_id =?`
 	}
 	defer func() { _ = rows.Close() }()
 
-	// Read all fountains in the result set
+	// Read all photos in the result set
 	for rows.Next() {
 		var p Photo_db
 		err = rows.Scan(&p.UserId, &p.PhotoId, &p.Timestamp, &p.Caption, &p.Image)
 		if err != nil {
 			return []Photo_db{}, err
 		}
-		ret = append(ret, p)
+		photos = append(photos, p)
 	}
 	if err = rows.Err(); err != nil {
 		return []Photo_db{}, err
 	}
 
-	return ret, nil
+	return photos, nil
 
 }
