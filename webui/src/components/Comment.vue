b582110d5db5a58ba6cb541c3a21c312963e5b0d
Alessandro Vecchi <vecchi.1938069@studenti.uniroma1.it>
2023-01-26 20:43:32 +0100

    openApi adjusted

--- webui/src/components/Comment.vue
+++ webui/src/components/Comment.vue
@@ -198,7 +198,6 @@ export default {
                 <!-- It's known that the majority have suffered alteration in some form, by injected humour, or randomised
                 words. I wonder what happens if I write anothe thing. Interesting, so it keeps increasing till we reach the end of the line. what happens now? -->
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300&display=swap');
 @import url('https://fonts.googleapis.com/css?family=Proxima+Nova');
 
 .media {
